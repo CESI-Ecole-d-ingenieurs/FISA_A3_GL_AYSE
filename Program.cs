145d17f8b6e4177849fed3464ae95de5ff297c6c
Aysee2 <ayse.deniz@viacesi.fr>
2025-02-03 11:31:07 +0100

    Ajout de la logique de log et de l'ajout des logs en temps réel

--- Program.cs
+++ Program.cs
@@ -5,3 +5,34 @@ Console.WriteLine("Hello, World!");
 GestionnaireDeSauvegarde s= new GestionnaireDeSauvegarde();
 //s.ExecuterSauvegarde("C:\\Users\\salem\\OneDrive\\Bureau\\Aya", "C:\\Users\\salem\\OneDrive\\Bureau\\SALEM");
 s.ParseJobIndices("1-4-6");
+
+
+Console.Write("Entrez le nom de la sauvegarde: ");
+string name = Console.ReadLine();
+Console.Write("Entrez le chemin du répertoire source: ");
+string sourceDirectory = Console.ReadLine();
+Console.Write("Entrez le chemin du répertoire cible: ");
+string targetDirectory = Console.ReadLine();
+
+
+//ici simulation de l'avancement de la sauvegarde, pour vraie sauvegarde, enlever le for (le calcul de la progression est faite dans la méthode CalculerEtat)
+try
+{
+    EtatSauvegarde etat = EtatSauvegardeJournal.CalculerEtat(name, sourceDirectory, targetDirectory);
+    EtatSauvegardeJournal.MettreAJourEtat(etat);
+    Journalisation.Instance.EcrireLog(name, sourceDirectory, targetDirectory, etat.TotalFilesSize, 0.0);
+    Console.Write("kjbc");
+
+    for (int i = 0; i < etat.TotalFilesToCopy; i++)
+    {
+        EtatSauvegardeJournal.MettreAJourProgression(name);
+        //if (etat.NbFilesLeftToDo == 0)
+        //{
+        //    Journalisation.Instance.EcrireLog(name, sourceDirectory, targetDirectory, etat.TotalFilesSize, 0.0);
+        //}
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erreur: {ex.Message}");
+}
