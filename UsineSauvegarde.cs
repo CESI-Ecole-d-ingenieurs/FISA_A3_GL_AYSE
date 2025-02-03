75478a5a8c3b70507d0b8dfbe3d689ca2b8e261b
Ayasalem127 <aya.salem@viacesi.fr>
2025-02-03 11:03:42 +0100

    test

---
+++ UsineSauvegarde.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetV0._1
+{
+    public interface UsineSauvegarde
+    {
+         StrategieSauvegarde CreerStrategieSauvegarde();
+    }
+    public class UsineSauvegardeComplete :UsineSauvegarde
+    {
+        public StrategieSauvegarde CreerStrategieSauvegarde()
+        {
+            return new StrategieSauvegardeComplete();
+        }
+    }
+    public class UsineSauvegardeDiff : UsineSauvegarde
+    {
+        public StrategieSauvegarde CreerStrategieSauvegarde()
+        {
+            return new StrategieSauvegardeDiff();
+        }
+    }
+}
