26cc19450f5932bb2906d0806eb41b15e79744fc
Ayasalem127 <aya.salem@viacesi.fr>
2025-02-03 13:39:18 +0100

    Test

---
+++ ProjetV0.1/Sauvegarde.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ProjetV0._1
+{
+    internal class Sauvegarde
+    {
+        public string Nom { get; set; }
+        public string Source { get; set; }
+        public string Destination { get; set; }
+        public string Type { get; set; }
+        public Sauvegarde(string nom, string source, string destination, string type)
+        {
+            Nom = nom;  
+            Source = source;
+            Destination = destination;
+            Type = type;
+        }
+    }
+}
---
+++ ProjetV0.1/ServiceSauvegarde.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetV0._1
+{
+    internal class ServiceSauvegarde
+    {
+        private StrategieSauvegarde _StrategieSauvegarde;
+        public ServiceSauvegarde(StrategieSauvegarde strategieSauvegarde)
+        {
+            _StrategieSauvegarde = strategieSauvegarde;
+        }
+        public void ExecuteSauvegarde(String Source, String Destination)
+        {
+            _StrategieSauvegarde.ExecuterSauvegarde(Source, Destination);
+        }
+    }
+}
