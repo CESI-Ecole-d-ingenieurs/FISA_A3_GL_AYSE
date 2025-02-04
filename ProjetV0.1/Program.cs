a1a01bf11f6ce0435c119f9725b313bdc9bebc6f
Aysee2 <ayse.deniz@viacesi.fr>
2025-02-04 10:21:58 +0100

    Ajout des design pattern pour log et Temps réel, et création de l'architecture MVC avec Modèle, Vue, Controller

--- ProjetV0.1/Program.cs
+++ ProjetV0.1/Program.cs
@@ -4,6 +4,9 @@ using ProjetV0._1;
 using System;
 using interactive_menus;
 using translation;
+using ProjetV0._1.Controleur;
+using ProjetV0._1.Modele;
+using ProjetV0._1.Vue;
 
 class Program
 {
@@ -16,7 +19,54 @@ class Program
 
         ActionChoice actions_menu = new ActionChoice();
         await actions_menu.ActionsMenu(translation);
+
+        //-------------------------------------------------------------
+        // TEST BACKUPSTATE
+        //-------------------------------------------------------------
+
+        // Initialization
+        ConsoleView view = new ConsoleView();
+        BackupStateJournal.AddObserver(view);
+        BackupController controller = new BackupController();
+
+        // Creating a backup
+        var backup = controller.CreateBackup("Backup1", "C:\\Source", "D:\\Backup");
+
+        // Updating progress
+        for (int i = 0; i < backup.TotalFilesToCopy; i++)
+        {
+            controller.UpdateProgress("Backup1");
+            System.Threading.Thread.Sleep(500); // Simulate processing delay
+        }
+
+        Console.WriteLine("Backup completed.");
+
+        //-------------------------------------------------------------
+        // TEST LOG
+        //-------------------------------------------------------------
+
+        LogController logController = new LogController();
+            LogView logView = new LogView();
+
+            //Backup backup = new Backup
+            //{
+            //    Name = "Project Backup",
+            //    SourcePath = "C:/Project",
+            //    DestinationPath = "D:/Backup"
+            //};
+
+            long fileSize = 1048576; // 1MB
+            double transferTime = 2.5; // 2.5s
+
+            // Add a log entry
+            //logController.AddLog(backup, fileSize, transferTime);
+
+            // Display logs
+            string logFile = $"backup_log_{DateTime.Today:dd-MM-yyyy}.json";
+            logView.DisplayLog(logFile);
     }
+
+
 }
 //GestionnaireDeSauvegarde s= new GestionnaireDeSauvegarde();
 ////s.ExecuterSauvegarde("C:\\Users\\salem\\OneDrive\\Bureau\\Aya", "C:\\Users\\salem\\OneDrive\\Bureau\\SALEM");
