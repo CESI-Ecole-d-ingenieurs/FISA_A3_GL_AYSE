0d105f1b985841cfc5d72650507c37468cf039e8
Lisa JUNG <lisa.jung@viacesi.fr>
2025-02-16 11:50:00 +0100

    Ajout interface graphique + bouttons sans logique

--- ProjetWPF/MainWindow.xaml.cs
+++ ProjetWPF/MainWindow.xaml.cs
@@ -20,5 +20,65 @@ namespace ProjetWPF
         {
             InitializeComponent();
         }
+
+        private void ShowSettings(object sender, RoutedEventArgs e)
+        {
+            Settings.Visibility = Visibility.Visible;
+            Language.Visibility = Visibility.Collapsed;
+            Creation.Visibility = Visibility.Collapsed;
+            Execution.Visibility = Visibility.Collapsed;
+            Logs.Visibility = Visibility.Collapsed;
+        }
+
+        /*private void FileEncryptTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FileEncryptPlaceholder.Visibility = string.IsNullOrWhiteSpace(FileEncryptTextBox.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void BusinessSoftwareTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            BusinessSoftwarePlaceholder.Visibility = string.IsNullOrWhiteSpace(BusinessSoftwareTextBox.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }*/
+
+        private void ShowLanguage(object sender, RoutedEventArgs e)
+        {
+            Language.Visibility = Visibility.Visible;
+            Settings.Visibility = Visibility.Collapsed;
+            Creation.Visibility = Visibility.Collapsed;
+            Execution.Visibility = Visibility.Collapsed;
+            Logs.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowCreation(object sender, RoutedEventArgs e)
+        {
+            Creation.Visibility = Visibility.Visible;
+            Settings.Visibility = Visibility.Collapsed;
+            Language.Visibility = Visibility.Collapsed;
+            Execution.Visibility = Visibility.Collapsed;
+            Logs.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowExecution(object sender, RoutedEventArgs e)
+        {
+            Execution.Visibility = Visibility.Visible;
+            Creation.Visibility = Visibility.Collapsed;
+            Settings.Visibility = Visibility.Collapsed;
+            Language.Visibility = Visibility.Collapsed;
+            Logs.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowLogs(object sender, RoutedEventArgs e)
+        {
+            Logs.Visibility = Visibility.Visible;
+            Execution.Visibility = Visibility.Collapsed;
+            Creation.Visibility = Visibility.Collapsed;
+            Settings.Visibility = Visibility.Collapsed;
+            Language.Visibility = Visibility.Collapsed;
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
