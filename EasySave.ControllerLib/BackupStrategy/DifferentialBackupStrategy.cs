8155fb4f1c8ea36946c347d48e17e0f77192de3e
Hatice Yildiz <hatice.yildiz@viacesi.fr>
2025-02-19 11:58:27 +0100

    mise a jour

--- EasySave.ControllerLib/BackupStrategy/DifferentialBackupStrategy.cs
+++ EasySave.ControllerLib/BackupStrategy/DifferentialBackupStrategy.cs
@@ -37,7 +37,7 @@ namespace EasySave.ControllerLib.BackupStrategy
             {
                 var targetFile = file.Replace(source, target);
 
-                DateTime lastBackupTime = DateOfLastBackup(Logger.Logger.GetLogFileName(), file, targetFile);
+                DateTime lastBackupTime = await DateOfLastBackup(Logger.Logger.GetLogFileName(), file, targetFile);
                 var fileInfo = new FileInfo(file);
 
                 if (fileInfo.LastWriteTimeUtc > lastBackupTime)
@@ -49,21 +49,21 @@ namespace EasySave.ControllerLib.BackupStrategy
 
         /// Determines the last backup date of a given file based on the log history.
         /// It searches the log file to find the most recent backup date of the given file.
-        private DateTime DateOfLastBackup(string logFile, string source, string target)
+        private async Task<DateTime> DateOfLastBackup(string logFile, string source, string target)
         {
             DateTime lastBackupTime = DateTime.MinValue;
 
             // Vérifie si le fichier log n'existe pas ou est vide
             if (!File.Exists(logFile) || new FileInfo(logFile).Length == 0)
             {
-                Console.WriteLine("🚨 Aucun log trouvé, la sauvegarde différentielle copiera tous les fichiers.");
+                //Console.WriteLine(" Aucun log trouvé, la sauvegarde différentielle copiera tous les fichiers.");
                 return DateTime.MinValue; // Force la copie de tous les fichiers
             }
 
             string jsonLog = File.ReadAllText(logFile);
             if (string.IsNullOrWhiteSpace(jsonLog))
             {
-                Console.WriteLine("⚠️ Fichier log vide, la sauvegarde différentielle copiera tous les fichiers.");
+                //Console.WriteLine(" Fichier log vide, la sauvegarde différentielle copiera tous les fichiers.");
                 return DateTime.MinValue;
             }
 
@@ -88,7 +88,7 @@ namespace EasySave.ControllerLib.BackupStrategy
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"❌ Erreur lors de la lecture du fichier log : {ex.Message}");
+                Console.WriteLine (await Translation.Instance.Translate($" Erreur lors de la lecture du fichier log : {ex.Message}"));
                 return DateTime.MinValue;
             }
 
